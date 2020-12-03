add6ae27fa695d872f91d1c820e57d2d1eb8b6b0
Jonathan Frame <jmframe@crimson.ua.edu>
2020-12-03 12:12:31 -0700

    load trained model weights in lstm-python-run.py

--- lstm-python-run.py
+++ lstm-python-run.py
@@ -3,11 +3,12 @@ import pandas as pd
 import torch
 from torch import nn
 import pickle
+import matplotlib.pyplot as plt
 
 torch.manual_seed(0)
 
 class LSTM(nn.Module):
-    def __init__(self, input_size=11, hidden_layer_size=100, output_size=1):
+    def __init__(self, input_size=11, hidden_layer_size=64, output_size=1):
         super(LSTM, self).__init__()
         self.input_size = input_size
         self.hidden_layer_size = hidden_layer_size
@@ -26,32 +27,58 @@ class LSTM(nn.Module):
 
 model = LSTM()
 
-print(model)
+istart=63578
+iend=72338
+n = iend - istart
 
 data_dir = './data/'
-with open(data_dir+'sugar_creek_input.csv','r') as f:
+with open(data_dir+'sugar_creek_input_all.csv','r') as f:
     df = pd.read_csv(f)
-input_tensor = torch.tensor(df.values)
+input_tensor = torch.tensor(df.iloc[istart:iend,:].values)
 
 with open(data_dir+'sugar_creek_scaler.p', 'rb') as fb:
-    p = pickle.load(fb)
+    scalers = pickle.load(fb)
+with  open(data_dir+'sugar_creek_basin_data.csv', 'r') as f:
+    sug_crek = pd.read_csv(f)
+obs = list(sug_crek.iloc[istart:iend,-1])
 
-att_means = np.append(np.array(p['attribute_means'].values)[1:3], np.array(p['attribute_means'].values)[0])
-att_stds = np.append(np.array(p['attribute_stds'].values)[1:3], np.array(p['attribute_stds'].values)[0])
-scaler_mean = np.append(np.array(p['xarray_means'].to_array())[:-1], att_means)
-scaler_std = np.append(np.array(p['xarray_stds'].to_array())[:-1], att_stds)
+pretrained_dict = torch.load(data_dir+'sugar_creek_trained.pt')
+
+# Change the name of the "head" layer to linear, since that is what the LSTM expects
+pretrained_dict['linear.weight'] = pretrained_dict.pop('head.net.0.weight')
+pretrained_dict['linear.bias'] = pretrained_dict.pop('head.net.0.bias')
+
+model.load_state_dict(pretrained_dict)
+
+att_means = np.append(np.array(scalers['attribute_means'].values)[1:3], np.array(scalers['attribute_means'].values)[0])
+att_stds = np.append(np.array(scalers['attribute_stds'].values)[1:3], np.array(scalers['attribute_stds'].values)[0])
+scaler_mean = np.append(np.array(scalers['xarray_means'].to_array())[:-1], att_means)
+scaler_std = np.append(np.array(scalers['xarray_stds'].to_array())[:-1], att_stds)
 input_tensor = (input_tensor-scaler_mean)/scaler_std
 
-hidden_layer_size = 100
+hidden_layer_size = 64
 h_t = torch.zeros(1,1, hidden_layer_size).float()
 c_t = torch.zeros(1,1, hidden_layer_size).float()
-for t in range(300):
+output_list = []
+for t in range(n):
     output, h_t, c_t = model(input_tensor[t,:], h_t, c_t)
     with torch.no_grad():
-        print(output * np.array(p['xarray_stds'].to_array())[-1] + np.array(p['xarray_means'].to_array())[-1])
+        output = output * np.array(scalers['xarray_stds'].to_array())[-1] + np.array(scalers['xarray_means'].to_array())[-1]
+        output_list.append(output[0,0,0].numpy().tolist())
 
-
-
+diff_sum2 = 0
+diff_sum_mean2 = 0
+obs_mean = np.nanmean(np.array(obs))
+print(obs_mean)
+for i, j in zip(output_list, obs):
+    if np.isnan(j):
+        continue
+    mod_diff = i-j
+    mean_diff = j-obs_mean
+    diff_sum2 += np.power((mod_diff),2)
+    diff_sum_mean2 += np.power((mean_diff),2)
+nse = 1-(diff_sum2/diff_sum_mean2)
+print(nse)
 
 
 
