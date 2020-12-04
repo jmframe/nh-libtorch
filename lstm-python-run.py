f59c9103820ebee468856a9728c685b235ea4968
Jonathan Frame <jmframe@crimson.ua.edu>
2020-12-03 22:14:09 -0700

    nse score in lstm-python-run.py

--- lstm-python-run.py
+++ lstm-python-run.py
@@ -14,15 +14,15 @@ class LSTM(nn.Module):
         self.hidden_layer_size = hidden_layer_size
         self.batch_size = 1 # In this application we do one timestep only
         self.lstm = nn.LSTM(input_size, hidden_layer_size)
-        self.linear = nn.Linear(hidden_layer_size, output_size)
+        self.head = nn.Linear(hidden_layer_size, output_size)
 
     def forward(self, input_layer, h_t, c_t):
         h_t = h_t.float()
         c_t = c_t.float()
         input_layer = input_layer.float()
         input_view = input_layer.view(1,1,-1)
-        output, (c_t, h_t) = self.lstm(input_view, (h_t,c_t))
-        prediction = self.linear(h_t)
+        output, (h_t, c_t) = self.lstm(input_view, (h_t,c_t))
+        prediction = self.head(output)
         return prediction, h_t, c_t
 
 model = LSTM()
@@ -45,11 +45,13 @@ obs = list(sug_crek.iloc[istart:iend,-1])
 pretrained_dict = torch.load(data_dir+'sugar_creek_trained.pt')
 
 # Change the name of the "head" layer to linear, since that is what the LSTM expects
-pretrained_dict['linear.weight'] = pretrained_dict.pop('head.net.0.weight')
-pretrained_dict['linear.bias'] = pretrained_dict.pop('head.net.0.bias')
+pretrained_dict['head.weight'] = pretrained_dict.pop('head.net.0.weight')
+pretrained_dict['head.bias'] = pretrained_dict.pop('head.net.0.bias')
 
 model.load_state_dict(pretrained_dict)
 
+obs_std = np.array(scalers['xarray_stds'].to_array())[-1]
+obs_mean = np.array(scalers['xarray_means'].to_array())[-1]
 att_means = np.append(np.array(scalers['attribute_means'].values)[1:3], np.array(scalers['attribute_means'].values)[0])
 att_stds = np.append(np.array(scalers['attribute_stds'].values)[1:3], np.array(scalers['attribute_stds'].values)[0])
 scaler_mean = np.append(np.array(scalers['xarray_means'].to_array())[:-1], att_means)
@@ -63,14 +65,13 @@ output_list = []
 for t in range(n):
     output, h_t, c_t = model(input_tensor[t,:], h_t, c_t)
     with torch.no_grad():
-        output = output * np.array(scalers['xarray_stds'].to_array())[-1] + np.array(scalers['xarray_means'].to_array())[-1]
+        output = output * obs_std + obs_mean
         output_list.append(output[0,0,0].numpy().tolist())
 
 diff_sum2 = 0
 diff_sum_mean2 = 0
 obs_mean = np.nanmean(np.array(obs))
-print(obs_mean)
-for i, j in zip(output_list, obs):
+for i, j in zip(output_list[7000:], obs[7000:]):
     if np.isnan(j):
         continue
     mod_diff = i-j
@@ -78,7 +79,7 @@ for i, j in zip(output_list, obs):
     diff_sum2 += np.power((mod_diff),2)
     diff_sum_mean2 += np.power((mean_diff),2)
 nse = 1-(diff_sum2/diff_sum_mean2)
-print(nse)
+print('NSE score for {} timesteps = {:.2f}'.format(n, nse))
 
 
 
