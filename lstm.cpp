15f4ecd251c15996c78adebbdb4dd55c184e554e
Nels Frazier <nels.frazier@noaa.gov>
2020-12-08 06:36:24 -0800

    remove tensor pointer workaround for bug caused by push_back to resized vector in read_input

--- lstm.cpp
+++ lstm.cpp
@@ -6,19 +6,22 @@
 
 typedef std::unordered_map< std::string, std::unordered_map< std::string, double> > ScaleParams;
 
-ScaleParams read_scale_params()
+ScaleParams read_scale_params(std::string path)
 {
     //Read the mean and standard deviation and put them into a map
     ScaleParams params;
-    CSVReader reader("input_scaling.csv");
+    CSVReader reader(path);
     auto data = reader.getData();
     std::vector<std::string> header = data[0];
+    //Advance the iterator to the first data row (skip the header)
     auto row = data.begin();
     std::advance(row, 1);
+    //Loop form first row to end of data
     for(; row != data.end(); ++row)
     {
 	for(int i = 0; i < (*row).size(); i++)
-	{
+	{   //row has var, mean, std_dev
+ 	    //read into map keyed on var name, param name
     	    params[ (*row)[0] ]["mean"] = strtof( (*row)[1].c_str(), NULL); 
   	    params[ (*row)[0] ]["std_dev"] = strtof( (*row)[2].c_str(), NULL);
 	}
@@ -28,45 +31,47 @@ ScaleParams read_scale_params()
 }
 
 
-torch::Tensor* read_input(std::string path, int& nrows, int& ncols)
+std::vector<torch::Tensor> read_input(std::string path, std::string scale_path, int& nrows, int& ncols)
 {
     // Import forcing data and save in a vector of vectors. 
     // Columns are in order for trained NeuralHydrology code
     // LWDOWN, PSFC, Q2D, RAINRATE, SWDOWN, T2D, U2D, V2D, lat, lon, area_sqkm
+    // NOTE this order is important!!!
+    
     CSVReader reader(path); 
     std::vector<std::vector<std::string> > data_str = reader.getData();
     nrows = data_str.size();
     ncols = data_str[0].size();
     std::vector<torch::Tensor> out;
+    //Create nrows default (undefined) tensors
     out.resize(nrows);
-    ScaleParams scale = read_scale_params();
-
+    //get the scaling parameters
+    ScaleParams scale = read_scale_params(scale_path);
+    //get the header row
     auto header = data_str[0];
+    //variables to parse into
     double temp, mean, std_dev;
-    std::vector< double > data(ncols);
-    torch::Tensor* tensors = new torch::Tensor[nrows];
+    torch::Tensor t;
 
     for (int i = 1; i < nrows ; ++i) {
 	//init an empty tensor
-	//out.push_back( torch::zeros( {1, ncols}, torch::dtype(torch::kFloat64) ));
-	tensors[i] =  torch::zeros( {1, ncols}, torch::dtype(torch::kFloat64) );
+	t =  torch::zeros( {1, ncols}, torch::dtype(torch::kFloat64) );
         for (int j = 0; j < ncols; ++j){
+	    //data value as double
             temp = strtof(data_str[i][j].c_str(),NULL);
             mean = scale[ header[j] ]["mean"];
 	    std_dev = scale[ header[j] ]["std_dev"];
-	    data[j] = (temp - mean) / std_dev;
-	    tensors[i][0][j] =  (temp - mean) / std_dev;
+	    t[0][j] =  (temp - mean) / std_dev;
         }
-        //std::cout<<"HERE\n";
-        //std::cout<<data<<"\n";
-	//torch::Tensor t = torch::from_blob(data.data(), {1,ncols} , torch::dtype(torch::kFloat64));
-        //std::cout<<t<<"\n";	
-        //out.push_back( std::move( t ) );
-        //std::cout<<tensors[i]<<"\n";
+	out[i] = t;
+	//you can push data to a vector and crate the tensor from the blob, but have to clone it since the data would be scoped to the function
+	//so just create the empty tensor and fill it in, so the vector out owns the tensor pointer AND its data...
+	// torch::from_blob(data.data(), {1,ncols}, torch::dtype(torch::kFloat64)).clone();
     }
     
-    return tensors;
+    return out;
 }
+
 int main(int argc, char** argv) {
   if (argc < 2) {
     std::cerr <<"Please provide model name." << std::endl;
@@ -108,28 +113,28 @@ int main(int argc, char** argv) {
     std::cout << "importing sugar creek data \n";
     int nrows, ncols;
 
-    //std::vector<torch::Tensor> input_data = read_input("data/sugar_creek_input.csv", nrows, ncols);
-    torch::Tensor* input_data = read_input("data/sugar_creek_input.csv", nrows, ncols);
-    std::cout<<"Preparing input tensor with "<<nrows<<", "<<ncols<<"\n";   
-    // turn array into torch tensor
+    std::vector<torch::Tensor> input_data = read_input("data/sugar_creek_input.csv", "input_scaling.csv", nrows, ncols);
+
+    // create the rest of the input tensors
     auto options = torch::TensorOptions().dtype(torch::kFloat64).device(device);
-    //torch::Tensor input_tensor = torch::from_blob(input_arr, {nrows, ncols}, torch::dtype(torch::kFloat64)).to(device);
     torch::Tensor h_t = torch::zeros({1, 1, 64}, options);
     torch::Tensor c_t = torch::zeros({1, 1, 64}, options);
     torch::Tensor output = torch::zeros({1}, options);
-    std::cout<<"Created tensors, preparing for forward pass\n";
+    
     // Input to the model is a vector of "IValues" (tensors)
-//    std::vector<torch::jit::IValue> input = {input_tensor, h_t, c_t};
+    //    std::vector<torch::jit::IValue> input = {input_tensor, h_t, c_t};
     // `model.forward` does what you think it does; it returns an IValue
     // which we convert back to a Tensor
+    // Loop over each input
     for (int i = 1; i < nrows; ++i){
         std::vector<torch::jit::IValue> inputs;
-	//Why do this when we already have input_tensor above?
-        //torch::Tensor input_row = input_data[i].to(device);
-        inputs.push_back(input_data[i].to(device));
+        // Create the model input for one time step
+	inputs.push_back(input_data[i].to(device));
         inputs.push_back(h_t);
         inputs.push_back(c_t);
+	// Run the model
         auto output_tuple = model.forward(inputs);
+	//Get the outputs
         torch::Tensor output = output_tuple.toTuple()->elements()[0].toTensor();
         torch::Tensor h_t = output_tuple.toTuple()->elements()[1].toTensor();
         torch::Tensor c_t = output_tuple.toTuple()->elements()[2].toTensor();
@@ -137,8 +142,6 @@ int main(int argc, char** argv) {
 
     }
   
-    delete[] input_data;
- 
   }
   catch (const c10::Error& e) {
     std::cerr << "An error occured: " << e.what() << std::endl;
