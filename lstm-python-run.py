4859a95919cffba27b3ed05a7751886f1f12609f
sft-managed <u00u6fuebzvX0lZXvO357@cpu0547.raplab.sjc4.nvmetal.net>
2020-12-08 16:14:29 -0800

    good results from the pytorch LSTM for C++

--- lstm-python-run.py
+++ lstm-python-run.py
@@ -3,17 +3,17 @@ import pandas as pd
 import torch
 from torch import nn
 import pickle
-import matplotlib.pyplot as plt
-from tqdm import tqdm
+import matplotlib as plt
 
 torch.manual_seed(0)
 
 class LSTM(nn.Module):
-    def __init__(self, input_size=11, hidden_layer_size=64, output_size=1, batch_size=256):
+    def __init__(self, input_size=11, hidden_layer_size=64, output_size=1, batch_size=1, seq_length=1):
         super(LSTM, self).__init__()
         self.input_size = input_size
         self.hidden_layer_size = hidden_layer_size
-        self.batch_size = batch_size # In this application we do one timestep only
+        self.seq_length=seq_length
+        self.batch_size = batch_size # We shouldn't neeed to do a higher batch size.
         self.lstm = nn.LSTM(input_size, hidden_layer_size)
         self.linear = nn.Linear(hidden_layer_size, output_size)
 
@@ -21,26 +21,28 @@ class LSTM(nn.Module):
         h_t = h_t.float()
         c_t = c_t.float()
         input_layer = input_layer.float()
-        input_view = input_layer.view(batch_size, input_size)
-        input_layer = input_view.unsqueeze(1)
-        output, (h_t, c_t) = self.lstm(input_layer, (h_t,c_t))
-        prediction = self.linear(output)
+        input_view = input_layer.view(seq_length, batch_size, input_size)
+        output, (h_t, c_t) = self.lstm(input_view, (h_t,c_t))
+        prediction = self.linear(h_t)
         return prediction, h_t, c_t
 
 input_size = 11
 hidden_layer_size = 64
 output_size = 1
-batch_size = 336
-model = LSTM(input_size, hidden_layer_size, output_size, batch_size)
+seq_length = 336
+batch_size = 1
+model = LSTM(input_size, hidden_layer_size, output_size, batch_size, seq_length)
 
-istart=63578
+istart=71593
 iend=72338
-n = iend - istart
+warmup = seq_length
 
 data_dir = './data/'
 with open(data_dir+'sugar_creek_input_all.csv','r') as f:
     df = pd.read_csv(f)
-input_tensor = torch.tensor(df.iloc[istart:iend,:].values)
+df = df[['LWDOWN','PSFC','Q2D','RAINRATE','SWDOWN','T2D','U2D','V2D','area_sqkm', 'lat', 'lon']]  
+input_tensor = torch.tensor(df.values)
+print(input_tensor[istart-warmup,:])
 
 with open(data_dir+'sugar_creek_scaler.p', 'rb') as fb:
     scalers = pickle.load(fb)
@@ -48,46 +50,54 @@ with  open(data_dir+'sugar_creek_basin_data.csv', 'r') as f:
     sug_crek = pd.read_csv(f)
 obs = list(sug_crek.iloc[istart:iend,-1])
 
-pretrained_dict = torch.load(data_dir+'sugar_creek_trained.pt', map_location=torch.device('cpu'))
-# Change the name of the "head" layer to linear, since that is what the LSTM expects
-pretrained_dict['linear.weight'] = pretrained_dict.pop('head.net.0.weight')
-pretrained_dict['linear.bias'] = pretrained_dict.pop('head.net.0.bias')
-
-model.load_state_dict(pretrained_dict)
+p_dict = torch.load(data_dir+'sugar_creek_trained.pt', map_location=torch.device('cpu'))
+m_dict = model.state_dict()
+p_dict['linear.weight'] = p_dict.pop('head.net.0.weight')
+p_dict['linear.bias'] = p_dict.pop('head.net.0.bias')
+model.load_state_dict(p_dict)
 
 obs_std = np.array(scalers['xarray_stds'].to_array())[-1]
 obs_mean = np.array(scalers['xarray_means'].to_array())[-1]
-att_means = np.append(np.array(scalers['attribute_means'].values)[1:3], np.array(scalers['attribute_means'].values)[0])
-att_stds = np.append(np.array(scalers['attribute_stds'].values)[1:3], np.array(scalers['attribute_stds'].values)[0])
+att_means = np.append( np.array(scalers['attribute_means'].values)[0],np.array(scalers['attribute_means'].values)[1:3])
+att_stds = np.append(np.array(scalers['attribute_stds'].values)[0], np.array(scalers['attribute_stds'].values)[1:3])
 scaler_mean = np.append(np.array(scalers['xarray_means'].to_array())[:-1], att_means)
 scaler_std = np.append(np.array(scalers['xarray_stds'].to_array())[:-1], att_stds)
 input_tensor = (input_tensor-scaler_mean)/scaler_std
+print(input_tensor[istart-warmup,:])
 
 hidden_layer_size = 64
-seq_length = batch_size
-h_t = torch.zeros(1, 1, hidden_layer_size).float()
-c_t = torch.zeros(1, 1, hidden_layer_size).float()
+h_t = torch.zeros(1, batch_size, hidden_layer_size).float()
+c_t = torch.zeros(1, batch_size, hidden_layer_size).float()
 output_list = []
-for t in tqdm(range(seq_length+1,n)):
+for t in range(istart-warmup, iend):
     with torch.no_grad():
         input_layer = input_tensor[t-seq_length:t, :]
         output, h_t, c_t = model(input_layer, h_t, c_t)
-        output = output * obs_std + obs_mean
-        output_list.append(output[0,0,0].numpy().tolist())
+        if t >= istart:
+            output = output[0,0,0].numpy().tolist() * obs_std + obs_mean
+            output_list.append(output)
+
+print('output stats')
+print('mean', np.mean(output_list))
+print('min', np.min(output_list))
+print('max', np.max(output_list))
+print('observation stats')
+print('mean', np.nanmean(obs))
+print('min', np.nanmin(obs))
+print('max', np.nanmax(obs))
 
 diff_sum2 = 0
 diff_sum_mean2 = 0
 obs_mean = np.nanmean(np.array(obs))
-print(obs_mean)
-for i, j in zip(output_list, obs):
-    if np.isnan(j):
+count_samples = 0
+for j, k in zip(output_list, obs):
+    if np.isnan(k):
         continue
-    mod_diff = i-j
-    mean_diff = j-obs_mean
+    count_samples += 1
+    mod_diff = j-k
+    mean_diff = k-obs_mean
     diff_sum2 += np.power((mod_diff),2)
     diff_sum_mean2 += np.power((mean_diff),2)
 nse = 1-(diff_sum2/diff_sum_mean2)
-print(nse)
-
-
-
+print('Nash-Suttcliffe Efficiency', nse)
+print('on {} samples'.format(count_samples))
